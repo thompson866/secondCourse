2fd65edbfa5c173d781d5ad49e78c838e4f7d3f0
arxip <arxipov1989@gmail.com>
2023-06-11 14:51:08 +0300

    1

---
+++ src/main/java/pro/sky/courseWorkTwo/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.courseWorkTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkTwoApplication.class, args);
+	}
+
+}
